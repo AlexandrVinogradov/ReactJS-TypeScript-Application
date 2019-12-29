c6e1e1a3e1ea35be7da280dd87b2034fc39e49ae
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-12-29 19:41:08 +0300

    added react-router-dom

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AboutPage: React.FC = () => {
+  return <>
+  <h1>Inforation</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, consequatur accusamus rem dolorem dicta repudiandae quia molestiae nihil non voluptatum!</p>
+  <button className='btn'>Return to todos</button>
+  </>
+}
