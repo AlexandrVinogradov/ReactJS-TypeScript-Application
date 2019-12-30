90ff7912662ba65de6a293e995e17453e10aaa9e
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-12-30 12:09:17 +0300

    Updates

---
+++ precache-manifest.90774f1f96d82cc89a0d1ecaec939464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7418f84152f255f19d3861f33699e9d3",
+    "url": "/ReactJS-TypeScript-Application/index.html"
+  },
+  {
+    "revision": "69891436192de2b7c5a3",
+    "url": "/ReactJS-TypeScript-Application/static/css/main.c3e2637e.chunk.css"
+  },
+  {
+    "revision": "a39ae29631740cc82f16",
+    "url": "/ReactJS-TypeScript-Application/static/js/2.9499339b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ReactJS-TypeScript-Application/static/js/2.9499339b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69891436192de2b7c5a3",
+    "url": "/ReactJS-TypeScript-Application/static/js/main.276d7b58.chunk.js"
+  },
+  {
+    "revision": "0c1eea48754ca5b27ba3",
+    "url": "/ReactJS-TypeScript-Application/static/js/runtime-main.0e971213.js"
+  }
+]);
